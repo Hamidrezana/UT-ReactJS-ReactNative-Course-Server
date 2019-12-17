7a58e815c0bc30471b6f009154a43baf16758cce
Hamidreza Nazemi <hamidrezanazemi16@gmail.com>
2019-12-18 00:49:10 +0330

    DB

--- database/index.js
+++ database/index.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize')
 const UserModel = require('../models/user.model')
 const BlogModel = require('../models/blog.model')
 
-const sequelize = new Sequelize('reactCourseDB', 'hamidreza', '', {
-    host: 'localhost',
-    dialect: 'postgres',
-})
+// const sequelize = new Sequelize('reactCourseDB', 'hamidreza', '', {
+//     host: 'localhost',
+//     dialect: 'postgres',
+// })
+const sequelize = new Sequelize('postgres://lkddxxrdsufbew:42f43d50ee962c018615163581fb0f43e9dfd9af2de2cf45f2d76551211fe251@ec2-174-129-255-7.compute-1.amazonaws.com:5432/d7f0d1glfadm2j');
+
 
 const User = UserModel(sequelize, Sequelize)
 const Blog = BlogModel(sequelize, Sequelize)
