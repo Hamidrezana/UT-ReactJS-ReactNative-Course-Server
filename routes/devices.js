03965e18c5a8717a95d20afb9222047cbe2afa31
Hamidreza Nazemi <hamidrezanazemi16@gmail.com>
2019-12-21 14:21:22 +0330

    updated

--- routes/devices.js
+++ routes/devices.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router();
 const deviceController = require('../controllers/device')
 
-router.post('/add', deviceController.add);
+router.post('/add', deviceController.add)
+router.post('/get-all', deviceController.getAll)
 
 module.exports = router;
